489006f3808aea4961b444954ac685c94ef0df60
Carolina <carolinang1994@gmail.com>
2020-11-03 00:26:40 -0600

    Maqueta bankapp1.jpg

--- test-bankapp-conecta/src/app/app.module.ts
+++ test-bankapp-conecta/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovimientosComponent } from './componentes/movimientos/movimientos.component';
+import { SaldosComponent } from './componentes/saldos/saldos.component';
+import { TarjetasComponent } from './componentes/tarjetas/tarjetas.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimientosComponent,
+    SaldosComponent,
+    TarjetasComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
