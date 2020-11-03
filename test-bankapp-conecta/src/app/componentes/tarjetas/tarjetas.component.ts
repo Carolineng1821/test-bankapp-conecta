ceeaadf437b7bf5581b133f05638aa96f1d5728c
Carolina <carolinang1994@gmail.com>
2020-11-03 02:09:06 -0600

    Funcionalidad JSON

--- test-bankapp-conecta/src/app/componentes/tarjetas/tarjetas.component.ts
+++ test-bankapp-conecta/src/app/componentes/tarjetas/tarjetas.component.ts
@@ -10,18 +10,58 @@ import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
   styleUrls: ['./tarjetas.component.css']
 })
 export class TarjetasComponent implements OnInit {
-
+//  declaramos variables
   tarjeta: TarjetasModule [];
 
-  constructor(private cardService: CardService) { }
+  public form: FormGroup = this.fb.group({
+    ntar: [ ],
+    cta: [ ],
+    issu: [ ],
+    nom: [ ],
+    mar: [ ],
+    stat: [ ],
+    sld: [ ],
+    tcta: [ ],
+  });
+
+  constructor(private cardService: CardService, 
+     private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.form = new FormGroup({
+      firstName: new FormControl()
+   });
 
     // aqui consumimos la api
     this.cardService.getCards().subscribe((trj: any) =>{
       this.tarjeta = trj.tarjetas;
       console.log(this.tarjeta)
     })
+  }
+
+  // metodo que construye e imprime JSON en alerta
+  onSub(): void{
+    const ntar = (<HTMLInputElement>document.getElementsByName('ntar')[0]).value.trim();
+    const cta = (<HTMLInputElement>document.getElementsByName('cta')[0]).value.trim();
+    const issu = (<HTMLInputElement>document.getElementsByName('issu')[0]).value.trim();
+    const nom = (<HTMLInputElement>document.getElementsByName('nom')[0]).value.trim();
+    const mar = (<HTMLInputElement>document.getElementsByName('mar')[0]).value.trim();
+    const stat = (<HTMLInputElement>document.getElementsByName('stat')[0]).value.trim();
+    const sld = (<HTMLInputElement>document.getElementsByName('sld')[0]).value.trim();
+    const tcta = (<HTMLInputElement>document.getElementsByName('tcta')[0]).value.trim();
+    var tarj = [{
+      'NumeroTarjeta': ntar,
+      'Cuenta': cta,
+      'Issuer': issu,
+      'Nombre': nom,
+      'Marca': mar,
+      'Estatus': stat,
+      'Saldo': sld,
+      'TipoCuenta': tcta
+    }]
+    alert(JSON.stringify(tarj));
+    console.log(tarj);
   }
 
 }
